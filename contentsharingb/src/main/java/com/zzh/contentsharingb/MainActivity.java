7eb9ad16e587f1b06133b97c2234f69eb7d15678
zouzhihao1992 <zouzhihao_1992@163.com>
2016-05-29 18:00:42 +0800

    add file Sharing

---
+++ contentsharingb/src/main/java/com/zzh/contentsharingb/MainActivity.java
@@ -0,0 +1,40 @@
+package com.zzh.contentsharingb;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName() ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            Uri returnUri = data.getData();
+            Log.d(TAG,returnUri.toString());
+
+
+        }
+    }
+}
